b175d7e5a330c0d4af6516208f0a1ef5587444de
tsunat973 <t23am018@yamanashi.ac.jp>
2026-06-30 18:41:36 +0900

    Add clock face ticks using map

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,46 @@ function App() {
     return () => clearInterval(timerId);
   }, []);
 
+  // 時・分・秒から、それぞれの針の角度(度数)を計算する
+  const seconds = time.getSeconds();
+  const minutes = time.getMinutes();
+  const hours = time.getHours();
+
+  const secondAngle = seconds * 6; // 360度 / 60秒 = 6度ずつ
+  const minuteAngle = minutes * 6 + seconds * 0.1; // 分も秒に応じて少しずつ進む
+  const hourAngle = (hours % 12) * 30 + minutes * 0.5; // 360度 / 12時間 = 30度ずつ
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'Center', marginTop: '50px' }}>
+    //画面を中央に表示するための囲みdiv
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+      {/*svgの表示エリア svgは、図形を描くための特別なHTMLタグです。*/}
+      <svg width="300" height="300" viewBox="0 0 300 300">
+        {/* 時計の外枠 */}
+        <circle cx="150" cy="150" r="140" fill="white" stroke="black" strokeWidth="4" />
+        {/* 時針 */}
+        <line
+          x1="150" y1="150"
+          x2="150" y2="80"
+          stroke="black" strokeWidth="6"
+          transform={`rotate(${hourAngle}, 150, 150)`}
+        />
+        {/* 分針　*/}
+        <line x1="150" y1="150" x2="150" y2="50" stroke="black" strokeWidth="4"
+          transform={`rotate(${minuteAngle}, 150, 150)`} />
+        {/* 秒針 */}
+        <line x1="150" y1="150" x2="150" y2="40" stroke="red" strokeWidth="2"
+          transform={`rotate(${secondAngle}, 150, 150)`} />
+          {/*メモリを追加*/}
+          {Array.from({ length: 12 }).map((_, i) => {
+            const angle = i * 30; //12個を360°で等分
+            const x = 150 + 120 * Math.sin((angle * Math.PI) / 180);
+            const y = 150 - 120 * Math.cos((angle * Math.PI) / 180);
+            return <circle key = {i} cx = {x} cy = {y} r="4" fill="black" />;
+          })}
+        <circle cx="150" cy="150" r="5" fill="black" />
+      </svg>
+
+
     </div>
   )
 }
